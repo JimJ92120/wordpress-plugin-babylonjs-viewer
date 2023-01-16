9a814787b57fb6b27bdf1f7661d99f1b3ba9632a
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-01-16 13:50:19 +0700

    chore(init): add plugin entrypoint with headers

---
+++ babylonjs-viewer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:         Babylon.js Viewer
+ * 
+ * Description:         Allow users to view and add 3D models to their content with Babylon.js Viewer.
+ * Author:              JimJ92120
+ * Author URI:          https://github.com/JimJ92120
+ * 
+ * Version:             0.1.0
+ * Requires at least:   5.9
+ * Requires PHP:        7.4
+ */
