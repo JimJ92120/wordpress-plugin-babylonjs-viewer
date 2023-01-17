6cb4344fb18661aed84cd60480d97dc2d8c088b5
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-01-18 02:13:34 +0700

    feat(block > babylonjs-viewer): update url to model data and text

--- src/babylonjs-viewer/save.js
+++ src/babylonjs-viewer/save.js
@@ -2,14 +2,15 @@ import { useBlockProps } from "@wordpress/block-editor";
 import { Fragment } from "@wordpress/element";
 
 export default function save({ attributes }) {
-	const { url } = attributes;
+	const { model } = attributes;
+	const { title, url } = model;
 
 	return (
 		<div { ...useBlockProps.save() }>
 			{(url && url !== "") &&
 				<Fragment>
 					<p>
-						{ `Loading model from "${url}"`}
+						Rendering <b>{title}</b> from <em>{url}</em>
 					</p>
 					<div className="babylonjs-viewer" model={url}></div>
 				</Fragment>
