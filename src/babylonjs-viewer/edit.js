7a21a4393f982602a30165071ccca8442b6fa02e
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-01-19 00:02:03 +0700

    refactor(block > babylonjs-viewer): extract BlockControls area from edit.js

--- src/babylonjs-viewer/edit.js
+++ src/babylonjs-viewer/edit.js
@@ -1,104 +1,38 @@
 import { __ } from "@wordpress/i18n";
-import {
-  useBlockProps,
-  MediaUpload,
-  BlockControls,
-} from "@wordpress/block-editor";
-import {
-  TextControl,
-  Button,
-  ToolbarGroup,
-  ToolbarButton,
-} from "@wordpress/components";
-import { Fragment, useState } from "@wordpress/element"
-import {
-  edit as editIcon,
-} from '@wordpress/icons';
+import { useBlockProps } from "@wordpress/block-editor";
 
 import "./editor.scss";
+
 import BlockEditView from "./components/BlockEditView";
 import EditInspectorControls from "./components/EditInspectorControls";
+import EditBlockControls from "./components/EditBlockControls";
 
 export default function edit({ attributes, setAttributes }) {
   const { model, size } = attributes;
   const { title, url } = model;
   const { height, width } = size;
 
-  const [showEdit, setShowEdit] = useState(true);
-
   return (
     <div {...useBlockProps()}>
-      <BlockControls>
-        <ToolbarGroup>
-          <ToolbarButton
-            icon={editIcon}
-            label="Edit"
-            isPressed={showEdit}
-            onClick={() => setShowEdit(!showEdit)}
-          />
-        </ToolbarGroup>
-      </BlockControls>
-      {showEdit &&
-        <Fragment>
-          <Fragment>
-            <p>
-              {`Enter the model metas or `}
-              <MediaUpload
-                onSelect={({ title, url }) => {
-                  setAttributes({
-                    model: {
-                      title: title,
-                      url: url,
-                    },
-                  });
-                }}
-                multiple={false}
-                render={({ open }) => (
-                  <Fragment>
-                    <Button
-                      onClick={open}
-                      variant="link"
-                      style={{
-                        color: "white",
-                        padding: 0,
-                        fontSize: "inherit",
-                      }}
-                    >
-                      select from Media Library
-                    </Button>
-                  </Fragment>
-                )}
-              />
-            </p>
-          </Fragment>
-          <Fragment>
-            <TextControl
-              label="Title"
-              value={title}
-              onChange={(newValue) => {
-                setAttributes({
-                  model: {
-                    title: newValue,
-                    url: url,
-                  },
-                });
-              }}
-            />
-            <TextControl
-              label="URL"
-              value={url}
-              onChange={(newValue) => {
-                setAttributes({
-                  model: {
-                    title: title,
-                    url: newValue,
-                  }
-                });
-              }}
-            />
-          </Fragment>
-        </Fragment>
-      }
+      <EditBlockControls
+        model={model}
+        onTitleChange={(newValue) => {
+          setAttributes({
+            model: {
+              title: newValue,
+              url,
+            },
+          });
+        }}
+        onUrlChange={(newValue) => {
+          setAttributes({
+            model: {
+              title,
+              url: newValue,
+            },
+          });
+        }}
+      />
       <EditInspectorControls
         size={size}
         onHeightChange={(newValue) => {
