9b0dd56018c4e66a4cc1061ad218e1e8da27ee29
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-01-19 00:50:33 +0700

    chore(build): add build

--- build/babylonjs-viewer/index.asset.php
+++ build/babylonjs-viewer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '95c1b00973aa2aabc223');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '83fbd1b35504aa43b4c8');
